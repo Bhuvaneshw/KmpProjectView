a063b48a3a9333eef00851a19b21a109ab5791de
Bhuvaneshw <bhuvaneshwaran319@gmail.com>
2024-08-24 12:02:50 +0530

    Update Plugin name, Icon and Vendor details

--- src/main/kotlin/com/acutecoder/kmp/projectview/util/Constants.kt
+++ src/main/kotlin/com/acutecoder/kmp/projectview/util/Constants.kt
@@ -2,9 +2,9 @@ package com.acutecoder.kmp.projectview.util
 
 object Constants {
 
-    const val PANE_NAME = "Kmp Project"
+    const val PANE_NAME = "KMP Project"
     const val PANE_ID = "KmpProjectPane"
     const val PANE_WEIGHT = 11
-    const val SETTINGS_TAB_NAME = "Kmp Project View"
+    const val SETTINGS_TAB_NAME = "KMP Project View"
 
 }
