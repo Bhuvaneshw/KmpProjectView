d31a14383f41c2a4f2288a6ff5eb0905cc1463cb
Bhuvaneshw <bhuvaneshwaran319@gmail.com>
2024-08-25 21:39:43 +0530

    Only for gradle projects

--- src/main/kotlin/com/acutecoder/kmp/projectview/KmpProjectPane.kt
+++ src/main/kotlin/com/acutecoder/kmp/projectview/KmpProjectPane.kt
@@ -3,11 +3,13 @@ package com.acutecoder.kmp.projectview
 import com.acutecoder.kmp.projectview.preference.PreferenceObserver
 import com.acutecoder.kmp.projectview.util.Constants
 import com.acutecoder.kmp.projectview.util.KmpSelectInTarget
+import com.acutecoder.kmp.projectview.util.log
 import com.intellij.ide.SelectInTarget
 import com.intellij.ide.projectView.ProjectView
 import com.intellij.ide.projectView.impl.ProjectAbstractTreeStructureBase
 import com.intellij.ide.projectView.impl.ProjectViewPane
 import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.gradle.settings.GradleSettings
 
 class KmpProjectPane(private val project: Project) : ProjectViewPane(project) {
 
@@ -35,6 +37,17 @@ class KmpProjectPane(private val project: Project) : ProjectViewPane(project) {
 
     override fun getWeight(): Int {
         return Constants.PANE_WEIGHT
+    }
+
+    override fun isInitiallyVisible(): Boolean {
+        try {
+            val gradleSettings = GradleSettings.getInstance(project)
+            val linkedProjects = gradleSettings.linkedProjectsSettings
+            return linkedProjects.isNotEmpty()
+        } catch (e: Exception) {
+            log(e)
+            return true
+        }
     }
 
 }
