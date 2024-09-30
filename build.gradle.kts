7b123c3151e12fa21f6c87021b6ce8f8fe60b414
Bhuvaneshw <bhuvaneshwaran319@gmail.com>
2024-09-30 11:10:30 +0530

    Reset intellij version to 2023.2.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2024.2")
+  version.set("2023.2.6")
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf("org.jetbrains.plugins.gradle"))
