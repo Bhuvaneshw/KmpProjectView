a95db869e9950a8425edf85a9cda70cfb05e3fd1
Bhuvaneshw <bhuvaneshwaran319@gmail.com>
2024-11-22 21:24:08 +0530

    Set page weight Make Kmp Project View to last

--- src/main/kotlin/com/acutecoder/kmp/projectview/util/Constants.kt
+++ src/main/kotlin/com/acutecoder/kmp/projectview/util/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     const val PANE_NAME = "KMP Project"
     const val PANE_ID = "KmpProjectPane"
-    const val PANE_WEIGHT = 11
+    const val PANE_WEIGHT = 121
     const val SETTINGS_TAB_NAME = "KMP Project View"
     const val DEFAULT_WEIGHT = 10
 
